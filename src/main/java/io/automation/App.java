0c970301397612997da2021d8003a2294025c912
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2022-08-03 19:14:21 +1000

    added test command

--- src/main/java/io/automation/App.java
+++ src/main/java/io/automation/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello , from Container World!" );
+        System.out.println( "Hello Again, from Container World!" );
     }
 }
